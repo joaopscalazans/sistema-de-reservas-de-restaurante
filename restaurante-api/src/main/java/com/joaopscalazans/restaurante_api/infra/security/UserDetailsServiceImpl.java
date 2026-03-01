9c1bca7f03893cede5333558332535d4a3ecdac4
JoaoPSCalazans <joaopscalzans@gmail.com>
2026-03-01 17:53:13 -0300

    feat(user):cria a implementação UserDetailsService

---
+++ restaurante-api/src/main/java/com/joaopscalazans/restaurante_api/infra/security/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.joaopscalazans.restaurante_api.infra.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.joaopscalazans.restaurante_api.repository.UserRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService{
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return new UserDetailsImpl(
+             userRepository.findByEmail(username).orElseThrow()
+        );
+    }
+
+}
