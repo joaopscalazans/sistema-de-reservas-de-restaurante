a7abb444bff169bd1cd587aa5f7be256aa56a0ae
JoaoPSCalazans <joaopscalzans@gmail.com>
2026-03-01 17:35:48 -0300

    feat(user):implenta a camada controller mapeada em /user e os endpoints /login e /register

---
+++ restaurante-api/src/main/java/com/joaopscalazans/restaurante_api/controller/UserController.java
@@ -0,0 +1,41 @@
+package com.joaopscalazans.restaurante_api.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.joaopscalazans.restaurante_api.dto.LoginDTO;
+import com.joaopscalazans.restaurante_api.dto.RegisterDTO;
+import com.joaopscalazans.restaurante_api.dto.TokenDTO;
+import com.joaopscalazans.restaurante_api.infra.security.AuthService;
+import com.joaopscalazans.restaurante_api.service.UserService;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<TokenDTO> login(@RequestBody LoginDTO entity) {
+         return ResponseEntity.ok(new TokenDTO(authService.login(entity)));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<Void> register(@RequestBody RegisterDTO entity) {
+            userService.register(entity);
+            return ResponseEntity.noContent().build();
+    }
+    
+} 
+
+
