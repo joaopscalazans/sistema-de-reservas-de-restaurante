0d283b91ad8d01b3ac4d8c5cc15815c756a8e4de
JoaoPSCalazans <joaopscalzans@gmail.com>
2026-03-02 13:48:27 -0300

    feat(securityConfiguration): configura autorização dos endpoint de /dining-table e cria um metodo para inserir um admin

--- restaurante-api/src/main/java/com/joaopscalazans/restaurante_api/infra/security/SecurityConfiguration.java
+++ restaurante-api/src/main/java/com/joaopscalazans/restaurante_api/infra/security/SecurityConfiguration.java
@@ -1,7 +1,10 @@
 package com.joaopscalazans.restaurante_api.infra.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,33 +13,57 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.joaopscalazans.restaurante_api.domain.user.User;
+import com.joaopscalazans.restaurante_api.domain.user.UserRole;
+import com.joaopscalazans.restaurante_api.repository.UserRepository;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
 
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http){
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
         return http
-        .csrf(csrf -> csrf.disable())
-        .headers(headers -> headers.frameOptions(frame -> frame.sameOrigin()))
-        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-        .authorizeHttpRequests(auth -> auth
-            .requestMatchers("/h2-console/**").permitAll()
-            .requestMatchers("/user/**").permitAll()
-            .anyRequest().authenticated()
-        )
-        .build();
+                .csrf(csrf -> csrf.disable())
+                .headers(headers -> headers.frameOptions(frame -> frame.sameOrigin()))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/h2-console/**").permitAll()
+                        .requestMatchers("/user/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/dining-table").hasAnyRole("CLIENT","ADMIN")
+                        .requestMatchers("/dining-table/**").hasRole("ADMIN")
+                        .anyRequest().authenticated())
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration){
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) {
         return authenticationConfiguration.getAuthenticationManager();
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CommandLineRunner createAdmin(UserRepository userRepository, PasswordEncoder encoder) {
+        return args -> {
+            if (userRepository.findByEmail("admin@email.com").isEmpty()) {
+                User admin = new User();
+                admin.setName("Administrador");
+                admin.setEmail("admin@email.com");
+                admin.setPassword(encoder.encode("123456"));
+                admin.setRole(UserRole.ADMIN);
+                userRepository.save(admin);
+            }
+        };
     }
 
 }
