7cb855725d8460574a98ef484976d50c0da7f1a5
JoaoPSCalazans <joaopscalzans@gmail.com>
2026-03-03 15:07:41 -0300

    feat(reserve): adiciona a camada respository metodos de findByUser_Id e findAllConflicts

--- restaurante-api/src/main/java/com/joaopscalazans/restaurante_api/repository/ReserveRepository.java
+++ restaurante-api/src/main/java/com/joaopscalazans/restaurante_api/repository/ReserveRepository.java
@@ -1,9 +1,25 @@
 package com.joaopscalazans.restaurante_api.repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.joaopscalazans.restaurante_api.domain.reserve.Reserve;
 
+
 public interface ReserveRepository extends JpaRepository<Reserve, Long> {
+
+    List<Reserve> findByUser_Id(UUID user);
+
+    @Query("""
+        SELECT r FROM Reserve r 
+        WHERE r.diningTable.id = :tableId
+        AND r.status = 'ACTIVED'
+        AND r.date BETWEEN :startMinusHours AND :end
+        """)
+    List<Reserve> findAllConflicts(Long tableId,LocalDateTime startMinusHours, LocalDateTime end);
 
 }
